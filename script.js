adcc6ba1045b700819008b5809794f77ee6dcd00
unknown <harrisondjon1994@gmail.com>
2022-11-23 20:03:26 -0800

    updated js file and html file

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+
+let timeSlots = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+
+$(document).ready(function() {
+  $('.saveBtn').on('click', function() {
+    const description = $(this).siblings('.description').val(); // using the id of description as the key, .val 
+    const time = $(this).parent().attr('id'); // using id of time as the key
+
+    // saving the time and description in set item
+    localStorage.setItem(description, time);
+  }) 
+
+  let descriptionBox = $('.textarea')
+  for (let i = 0; i < descriptionBox.length; i++) {
+    const element = array[i];
+    
+  }
+
+  // id is renaming the my calendarTimeIds and also putting it in an array
+  // get item is passing the key 
+  timeSlots.forEach(setHour => {
+    $(`#${setHour} .description`).val(localStorage.getItem(`${setHour}`));   
   });
+});
+
   
+// /*homework help create a script
+// this is html
+// look at styling in css and use it as a guide when setting classes for the divs
+// row, a, etc.
+// div class = row time-block id = 9 oclock (passing jquery through it.) this is main div
+//     div, col-md-1 hour (9AM in the tags) this is inside the main div,; descriptions. save button. use autocompletion front font awesome
   
-  /*homework help create a script
-  this is html
-  look at styling in css and use it as a guide when setting classes for the divs
-  row, a, etc.
-  div class = row time-block id = 9 oclock (passing jquery through it.) this is main div
-      div, col-md-1 hour (9AM in the tags) this is inside the main div,
-  
-   decription. save button. use autocompetin from font awesome. material ui is another "bootstrap" website
-*/
+
